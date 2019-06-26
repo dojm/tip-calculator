66db163a3a3ba84bbc0d17538f15258fa7a9aa5a
Mary Do <marydo318@gmail.com>
2019-06-26 01:38:50 -0700

    added functions, updated html

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 const billAmount = document.getElementById('bill-amount');
-let tipPercentage = document.getElementById('tip-percentage');
+const tipPercentage = document.getElementById('tip-percentage');
+const calculatedTip = document.getElementById('calculated-tip');
+const output = document.getElementById('output');
 
-const calculateTip = (e) => {
-  
 
-  e.preventDefault(); 
+const calculateTip = () => {
+  let calculatedValue = billAmount.value * tipPercentage.value;
+  calculatedTip.value = calculatedValue.toFixed(2);
+  output.style.display = 'block';
+  removeLoader();
 }
 
-document.getElementById('calculator').addEventListener('submit', calculateTip);
+const loader = () => {
+  if(output.parentElement.querySelector('img') === null) {
+    let img = document.createElement('img');
+    img.src = 'ajax-loader.gif';
+    img.id = 'loader';
+    output.parentElement.appendChild(img);
+    img.style.display = 'block';
+    img.style.margin = '0 auto';
+  }
+}
+
+const removeLoader = () => {
+    const loader = document.getElementById('loader');
+    output.parentElement.removeChild(loader);
+}
+
+document.getElementById('calculator').addEventListener('submit', (e) => {
+  output.style.display = 'none';
+  loader();
+  setTimeout(calculateTip, 700);
+
+  e.preventDefault();
+});
