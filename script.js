d21c0384affddd0ffbb3e1414de48ca1be7ea4d5
Mary Do <marydo318@gmail.com>
2019-06-26 00:14:24 -0700

    fixed ui

---
+++ script.js
@@ -0,0 +1,10 @@
+const billAmount = document.getElementById('bill-amount');
+let tipPercentage = document.getElementById('tip-percentage');
+
+const calculateTip = (e) => {
+  
+
+  e.preventDefault(); 
+}
+
+document.getElementById('calculator').addEventListener('submit', calculateTip);
